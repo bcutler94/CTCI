48d177b15c949a68f6f3baf28ceb740fba5d5d13
Ben Cutler <bcutler94@gmail.com>
2019-05-29 19:22:53 -0400

    Add isUnique

--- arraysAndStrings/isUnique.js
+++ arraysAndStrings/isUnique.js
@@ -1,7 +1,10 @@
 // Implement algo to see if chars of string are unique, do it with and without
 // data structures
 
-function isUnique (string) {
+// with set
+// time complexity: O(s) -> s = length of string
+// space complexity: O(s) -> s = length of string
+function isUniqueWithSet (string) {
     const set = new Set ();
     for (let char of string) {
         if (set.has(char)) return false;
@@ -9,3 +12,13 @@ function isUnique (string) {
     }
     return true;
 }
+
+function isUniqueWithoutSet (string) {
+    const arr = string.split('').sort();
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] === arr[i + 1]) return false;
+    }
+    return true;
+}
+
+console.log(isUniqueWithoutSet('ben'))
