19c55d1ea912b3437a0bc5be64fdae569f2ce819
Ben Cutler <bcutler94@gmail.com>
2019-05-30 16:05:39 -0400

    Add oneAway

---
+++ arraysAndStrings/oneAway.js
@@ -0,0 +1,9 @@
+/*
+There are three types of edits that can be performed on strings: insert a char,
+remove a char or replace a char. Given two strings, determine if they are one
+edit away from being equal
+*/
+
+function oneAway (str1, str2) {
+    
+}
