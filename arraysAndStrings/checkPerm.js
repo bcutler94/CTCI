79ae13ad9387e7a53c2474fb9549a515fb6e6cb1
Ben Cutler <bcutler94@gmail.com>
2019-05-30 07:34:15 -0400

    Add checkPerm

--- arraysAndStrings/checkPerm.js
+++ arraysAndStrings/checkPerm.js
@@ -1,1 +1,12 @@
 // Given two strings, check whether one is a permutation of the other
+
+// with obj
+function checkPermWithObj (str1, str2) {
+    const obj1 = {};
+    const obj2 = {};
+    if (str1.length !== str2.length) return false;
+    for (let i = 0; i < str1.length; i++) {
+        obj1[str1[i]] = obj1[str1[i]] ? obj1[str1[i]]++ : 1;
+        obj2[str2[i]] = obj2[str2[i]] ? obj2[str2[i]]++ : 1;
+    }
+}
