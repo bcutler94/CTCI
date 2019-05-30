1fa35d45dfa9ced407371a730c0f6d69d080275f
Ben Cutler <bcutler94@gmail.com>
2019-05-30 11:34:17 -0400

    Add palindromePermutation

--- arraysAndStrings/palindromePermutations.js
+++ arraysAndStrings/palindromePermutations.js
@@ -1,1 +1,15 @@
 // Given a string determine whether it is a permutation of a palindrome
+
+function palindromePerm (str) {
+    const newStr = str.replace(/\s/g, '').toLowerCase();
+    const freqObj = {};
+    for (let char of newStr) {
+        freqObj[char] = freqObj[char] ? freqObj[char]++ : 1;
+    }
+    let oddCount = 0;
+    for (let key in freqObj) {
+        if (freqObj[key] % 2 === 1) oddCount++;
+        if (oddCount > 1) return false;
+    }
+    return true;
+}
