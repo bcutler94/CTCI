3485473c5c834dfb0c93ac2873ddfcd6b4518831
Ben Cutler <bcutler94@gmail.com>
2019-05-30 08:34:24 -0400

    Add urlify

---
+++ arraysAndStrings/urlify.js
@@ -0,0 +1,13 @@
+// Write a method to replace all spaces in a string with '%20', you are given
+// the true string length
+
+function urlify (str, len) {
+    const regex = /\s/;
+    for (let i = 0; i < str.length; i++) {
+        if (!regex.test(str[i])) {
+            let newStr = '';
+            let count = 0;
+            while
+        }
+    }
+}
