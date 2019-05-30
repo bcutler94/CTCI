635a18a451ce6082bb71ba2c903e6d283a7e7e1e
Ben Cutler <bcutler94@gmail.com>
2019-05-30 18:38:54 -0400

    Add rotateMatrix

--- arraysAndStrings/rotateMatrix.js
+++ arraysAndStrings/rotateMatrix.js
@@ -6,8 +6,15 @@ function rotateMatrix (matrix) {
     let i = 0;
     while (i < len) {
         j = len - 1;
+        const level = [];
         while (j >= 0) {
-            
+            level.push(matrix[j][i]);
+            j--;
         }
+        out.push(level);
+        i++;
     }
+    return out;
 }
+
+console.log(rotateMatrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
