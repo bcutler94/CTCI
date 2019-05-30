be655a86199d5fc1ea536d12bd13ad25b02738b0
Ben Cutler <bcutler94@gmail.com>
2019-05-30 18:36:36 -0400

    Add rotateMatrix

---
+++ arraysAndStrings/rotateMatrix.js
@@ -0,0 +1,13 @@
+// Rotate a matrix 90 degrees
+
+function rotateMatrix (matrix) {
+    const len = matrix.length;
+    const out = [];
+    let i = 0;
+    while (i < len) {
+        j = len - 1;
+        while (j >= 0) {
+            
+        }
+    }
+}
