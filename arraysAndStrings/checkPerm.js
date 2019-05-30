5b8007aa8dc1f4a35abf1c39f1d97cb83386f3c7
Ben Cutler <bcutler94@gmail.com>
2019-05-30 07:37:38 -0400

    Add checkPerm

--- arraysAndStrings/checkPerm.js
+++ arraysAndStrings/checkPerm.js
@@ -9,4 +9,13 @@ function checkPermWithObj (str1, str2) {
         obj1[str1[i]] = obj1[str1[i]] ? obj1[str1[i]]++ : 1;
         obj2[str2[i]] = obj2[str2[i]] ? obj2[str2[i]]++ : 1;
     }
+    for (let key in obj1) {
+        if (obj1[key] !== obj2[key]) return false;
+    }
+    return true;
+}
+
+// without obj
+function checkPerm (str1, str2) {
+    const sort1 = 
 }
