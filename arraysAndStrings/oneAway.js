e029dd89a3b95f4987f3cb20318d546756152f62
Ben Cutler <bcutler94@gmail.com>
2019-05-30 16:24:39 -0400

    Edit oneAway

--- arraysAndStrings/oneAway.js
+++ arraysAndStrings/oneAway.js
@@ -4,6 +4,8 @@ remove a char or replace a char. Given two strings, determine if they are one
 edit away from being equal
 */
 
+// time complexity: O(N + M) -> N and M are lengths of strings
+// space complexity: O(N) -> N = length of str1
 function oneAway (str1, str2) {
     if (Math.abs(str1.length - str2.length) > 1) return false;
     let maxDiff = Math.max(str1.length, str2.length) - 1;
