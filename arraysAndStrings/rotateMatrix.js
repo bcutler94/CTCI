8233b587aabaeb12efe4c9219ddf45bafa487101
Ben Cutler <bcutler94@gmail.com>
2019-05-30 20:20:34 -0400

    Add rotateMatrix

--- arraysAndStrings/rotateMatrix.js
+++ arraysAndStrings/rotateMatrix.js
@@ -21,7 +21,7 @@ function rotateMatrix (matrix) {
 function rotateMatrixConstantSpace (matrix) {
     let j = 0;
     const len = matrix.length - 1;
-    while (j < len - 1) {
+    while (j < len) {
         let i = 0;
         while (i < len - j) {
             if (len % 2 === 1) {
@@ -30,10 +30,10 @@ function rotateMatrixConstantSpace (matrix) {
                 if (j + 1 > len - i - j) return matrix;
             }
             const tLeft = matrix[j][j + i];
-            const tRight = matrix[j + 1][len - j];
+            const tRight = matrix[j + i][len - j];
             const bRight = matrix[len - j][len - i - j];
             const bLeft = matrix[len - i - j][j];
-            matrix[j + 1][len - j] = tLeft;
+            matrix[j + i][len - j] = tLeft;
             matrix[len - j][len - i - j] = tRight;
             matrix[len - i - j][j] = bRight;
             matrix[j][j + i] = bLeft;
@@ -41,6 +41,8 @@ function rotateMatrixConstantSpace (matrix) {
         }
         j++;
     }
+    return matrix
 }
 
-console.log(rotateMatrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
+console.log(rotateMatrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+console.log(rotateMatrixConstantSpace([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
